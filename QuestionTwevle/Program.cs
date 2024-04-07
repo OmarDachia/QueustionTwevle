1604d5696a24e4f98f01837bee0c7546b6fea687
Omar Dachia <omardachia@gmail.com>
2024-04-07 12:45:21 +0100

    #12 done tooo

---
+++ QuestionTwevle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionTwevle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#12.	WAP to calculate the Gross Salary of an employee.\nAssume Gross Salary=Basic Salary+DA+HRA\nDA=40% of Basic Salary\nHRA=20% of Basic Salary");
+            Console.WriteLine("================================================================================================================================================");
+            Console.WriteLine("Enter your Basic Salary:->");
+            double salary = Convert.ToDouble(Console.ReadLine());
+            double DA = salary * (40.0 / 100.0);
+            double HRA = salary * (20.0 / 100.0);
+            Console.WriteLine(string.Format("The Gross salary is {0} with salary of {1} and Allowances - DA (40%)= {2} and HRA (20%)= {3}",(salary + DA + HRA), salary, DA,HRA));
+               
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+What is Gross Pay?
+Gross pay is the amount of money earned per paycheck before deductions are made. Typically, the gross pay amount includes an employee’s standard pay rate or salary, plus any overtime during a pay period.
+
+Whether an employer pays weekly, bi-weekly, or monthly, the definition of gross pay remains the same. The amount of compensation that an employee receives on each check may change.
